76fb7cc67901c9afb002f641cbc9cc3341b0090d
freitasdev <frtecnologiadigital@gmail.com>
2024-01-03 08:30:44 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include"Configs/Includes/header.php";
+?>
+
+<h1>Ola Mundo</h1>
+<?php
+include"Configs/Includes/footer.php";
+?>
---
+++ pacientes.php
@@ -0,0 +1,7 @@
+<?php
+include"Configs/Includes/header.php";
+?>
+<h1>Ola Mundo</h1>
+<?php
+include"Configs/Includes/footer.php";
+?>
